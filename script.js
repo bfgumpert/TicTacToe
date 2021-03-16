8a2077c884eb1c5c3e63876ab44357435e3f9211
Brian Gumpert <bfgumpert@gmail.com>
2021-03-16 01:54:59 -0700

    players can now add game markers, need to add  win condition next

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-    let boardState = ["X", "O", "X", "X", "O", "O", "X", "O", "X"];
+    let boardState = ["", "", "", "", "", "", "", "", ""];
     return {
         boardState
     };
@@ -11,7 +11,7 @@ const displayController = (() => {
             const boardSlot = document.createElement("div");
             boardSlot.id = "Slot" + i;
             boardSlot.classList = "boardSlot";
-            boardSlot.textContent = "x";
+            boardSlot.textContent = "";
             container.appendChild(boardSlot);
             if (i < 4) {boardSlot.style.borderTop = "0px";}
             if (i >= 7 && i <= 9) {boardSlot.style.borderBottom = "0px";}
@@ -32,16 +32,78 @@ const displayController = (() => {
         })
     }
 
+    const changeActivePlayer = (player1, player2) => {
+        if (player1.checkActive()) {
+            player1.disableActive();
+            player2.enableActive();
+        }
+        else {
+            player1.enableActive();
+            player2.disableActive();
+        }
+    }
+
+    const checkWinner = () => {
+        const board = gameBoard.boardState;
+        if (board[0] == board[1] == board[2] ||
+            board[3] == board[4] == board[5] ||
+            board[6] == board[7] == board[8] ||
+            board[0] == board[3] == board[6] ||
+            board[1] == board[4] == board[7] ||
+            board[2] == board[5] == board[8] ||
+            board[2] == board[4] == board[6] ||
+            board[0] == board[4] == board[8]) {
+            //set a new variable to gameover...set gameover true then add condition to evenlistner
+        }
+    }
+
+    const addGameMarkEvent = () => {
+        for (let i = 1; i < 10; i++) {
+            const slotNumber = document.getElementById("Slot" + i);
+            slotNumber.addEventListener("click", () => {
+                if (playerCircle.checkActive() && slotNumber.textContent == "") {
+                    gameBoard.boardState[slotNumber.id.slice(-1) - 1] = "O";
+                }
+                else if (playerCross.checkActive() && slotNumber.textContent == "") {
+                    gameBoard.boardState[slotNumber.id.slice(-1) - 1] = "X";
+                }
+                displayGameBoard();
+                changeActivePlayer(playerCross, playerCircle);
+            })
+        }
+    }
+
     return {
         htmlInitialize,
-        displayGameBoard
+        displayGameBoard,
+        changeActivePlayer,
+        addGameMarkEvent
     };
 })();
 
-const players = () => {
-    return {};
+const players = (type) => {
+    let active = false;
+    const checkActive = () => active;
+    const enableActive = () => active = true;
+    const disableActive = () => active = false;
+
+    return {
+        type,
+        checkActive,
+        enableActive,
+        disableActive
+    };
 }
 
 console.log(gameBoard.boardState);
 displayController.htmlInitialize();
 displayController.displayGameBoard();
+
+const playerCircle = players("O");
+const playerCross = players("X");
+
+playerCross.enableActive();
+
+displayController.addGameMarkEvent();
+
+console.log(playerCircle.type);
